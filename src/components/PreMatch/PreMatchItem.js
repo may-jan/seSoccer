9283dcf61e5afd388c51d0b9da5919f86cad60ff
sognociel <sognociel@gmail.com>
2022-11-30 18:33:18 +0900

    style: 클래스 네임 수정

--- src/components/PreMatch/PreMatchItem.js
+++ src/components/PreMatch/PreMatchItem.js
@@ -6,26 +6,34 @@ const PreMatchItem = ({ matchData }) => {
   return (
     <PreMatchItems>
       <PreMatchCountry>
-        <img src={matchData.homeTeamImg} alt={matchData.homeTeam} />
-        <div className="country">{matchData.homeTeam}</div>
+        <img
+          className="prematchitem__img"
+          src={matchData.homeTeamImg}
+          alt={matchData.homeTeam}
+        />
+        <div className="prematchitem__country">{matchData.homeTeam}</div>
       </PreMatchCountry>
-      <div className="match-info">
-        <div className="score">{matchData.homeTeamScore}</div>
-        <div className="date">
-          <div className="date-current">
+      <div className="prematchitem__match_info">
+        <div className="prematchitem__score">{matchData.homeTeamScore}</div>
+        <div className="prematchitem__date">
+          <div className="prematchitem__date_current">
             {matchData.isMatched ? "경기종료" : "경기전"}
           </div>
-          <div className="date-time">
+          <div className="prematchitem__date_time">
             {matchData.date}
             <br />
             {matchData.time}
           </div>
         </div>
-        <div className="score">{matchData.awayTeamScore}</div>
+        <div className="prematchitem__score">{matchData.awayTeamScore}</div>
       </div>
       <PreMatchCountry>
-        <img src={matchData.awayTeamImg} alt={matchData.awayTeam} />
-        <div className="country">{matchData.awayTeam}</div>
+        <img
+          className="prematchitem__img"
+          src={matchData.awayTeamImg}
+          alt={matchData.awayTeam}
+        />
+        <div className="prematchitem__country">{matchData.awayTeam}</div>
       </PreMatchCountry>
     </PreMatchItems>
   );
