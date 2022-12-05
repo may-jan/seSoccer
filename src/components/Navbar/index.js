30aeafd1171c31bcc9b7b8058fd2bb955d251cfc
Jaebin <wjdwoqls777@naver.com>
2022-12-05 20:13:37 +0900

    feat: Nav 컴포넌트 데스크탑 스타일링 완료

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { NavContainer } from "./style";
+import { NavContainer, SidebarList } from "./style";
+import "./Sidebar.scss";
+import Logo from "../../asset/images/sesoccer.png";
 
 const Navbar = () => {
   return (
     <NavContainer>
-      <ul>
-        <li>
-          <Link to="/">home</Link>
-        </li>
-        <li>
-          <Link to="/vote">vote</Link>
-        </li>
-        <li>
-          <Link to="/mypage">mypage</Link>
-        </li>
-      </ul>
+      <div className="navbar">
+        <img className="seSoccerLogo" src={Logo} alt="Logo" />
+        <button className="loginButton">Login</button>
+      </div>
+
+      <div className="sidebar">
+        <SidebarList to="/">Home</SidebarList>
+        <SidebarList to="/vote">Vote</SidebarList>
+        <SidebarList to="/mypage">MyPage</SidebarList>
+      </div>
     </NavContainer>
   );
 };
--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const NavContainer = styled.div`
   flex-direction: column;
@@ -8,4 +9,26 @@ export const NavContainer = styled.div`
   @media (max-width: 800px) {
     display: none;
   }
+`;
+
+export const SidebarList = styled(Link)`
+  color: white;
+  list-style: none;
+  text-decoration: none;
+  text-align: center;
+  font-size: 30px;
+  height: 50px;
+  max-height: 100vh;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+
+  &:hover {
+    background: #252831;
+    border-left: 7px solid white;
+  }
+
+  cursor: pointer;
 `;
