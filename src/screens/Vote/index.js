b8ae32ff4666937f5822118421de48a8581602c9
신동주 <sindongju@sindongjuui-MacBookPro.local>
2022-12-07 21:57:48 +0900

    feat: vote update 기능 추가작업 진행 중

--- src/screens/Vote/index.js
+++ src/screens/Vote/index.js
@@ -1,56 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import MatchBox from './MatchBox'
-import { VoteContainer, RoundButtonContainer, RoundButton } from './style'
+import React, { useState, useEffect } from "react";
+import MatchBox from "./MatchBox";
+import { VoteContainer, RoundButtonContainer, RoundButton } from "./style";
 
 const Vote = () => {
-  const [matches, setMatches] = useState([])
-  const [filteredData, setFilteredData] = useState([])
+  const [matches, setMatches] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
   const [rounds, setRounds] = useState([
-    { title: '16강' },
-    { title: '8강' },
-    { title: '4강' },
-    { title: '3-4위전' },
-    { title: '결승' },
-  ])
+    { title: "16강" },
+    { title: "8강" },
+    { title: "4강" },
+    { title: "3-4위전" },
+    { title: "결승" },
+  ]);
 
-  const [checkRound, setCheckRound] = useState('16강')
+  const [checkRound, setCheckRound] = useState("16강");
 
   // Read (조회)
   const getData = () => {
     let response = fetch(`http://localhost:5050/api/rounds`)
-      .then(res => res.json())
-      .then(data => setMatches(data.rounds))
-  }
+      .then((res) => res.json())
+      .then((data) => setMatches(data.rounds));
+  };
 
   // Update (수정)
-  const updateData = (route, item) => {
-    let response = fetch(`http://localhost:5050/${route}/${item.id}`, {
-      method: 'PUT', // HTTP 통신방식 : GET, POST, PUT, DELETE
+  const updateData = (roundId, item) => {
+    let response = fetch(`http://localhost:5050/api/rounds/${roundId}`, {
+      method: "PATCH", // HTTP 통신방식 : GET, POST, PUT, DELETE
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         ...item,
       }),
-    })
-  }
+    });
+  };
 
   function filterMatches(round) {
-    let filtered = matches.filter(match => match.round === round)
-    setFilteredData(filtered)
+    let filtered = matches.filter((match) => match.round === round);
+    setFilteredData(filtered);
   }
 
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   // console.log(matches)
 
-  const changeRound = e => {
-    setCheckRound(e.target.value)
-    filterMatches(e.target.value)
-  }
-  console.log(filteredData)
+  const changeRound = (e) => {
+    setCheckRound(e.target.value);
+    filterMatches(e.target.value);
+  };
+  console.log(filteredData);
 
   return (
     <VoteContainer>
@@ -60,17 +60,17 @@ const Vote = () => {
             key={idx}
             variant="outlined"
             value={round.title}
-            onClick={e => changeRound(e)}
+            onClick={(e) => changeRound(e)}
           >
             {round.title}
           </RoundButton>
         ))}
       </RoundButtonContainer>
-      {filteredData.map(match => (
+      {filteredData.map((match) => (
         <MatchBox key={match.id} data={match} />
       ))}
     </VoteContainer>
-  )
-}
+  );
+};
 
-export default Vote
+export default Vote;
